d0c732c0557639202f13246cc94009510e972379
Jackson Dahl <99228002+jackson-dahl42@users.noreply.github.com>
2024-04-23 15:24:00 +0000

    Program now checks if their is a problem with the requests

--- main.py
+++ main.py
@@ -6,25 +6,36 @@ import requests
 ip = input("enter ip address:")
 
 # Get location data from webAPI using IP address
-response = requests.get('https://ipgeolocation.abstractapi.com/v1/?api_key=08497c09676b46d1aeb921bc0f5481fa&ip_address=' + ip)
+response = requests.get(f'https://ipgeolocation.abstractapi.com/v1/?api_key=08497c09676b46d1aeb921bc0f5481fa&ip_address={ip}')
 
-# Get longtiude and latitude from response
-location = response.json()
-longitude = location['longitude']
-latitude = location['latitude']
+if response.status_code == 200:
+  
+  # Get longtiude and latitude from response
+  location = response.json()
+  longitude = location['longitude']
+  latitude = location['latitude']
+  
+  # Use longitude and latitude to get rain data from webAPI
+  response2 = requests.get(f'https://api.open-meteo.com/v1/forecast?latitude={str(latitude)}&longitude={str(longitude)}&hourly=rain')
 
-# Use longitude and latitude to get rain data from webAPI
-response2 = requests.get('https://api.open-meteo.com/v1/forecast?latitude=' + str(latitude) + '&longitude=' + str(longitude) + '&hourly=rain')
-data = response2.json()
-time = data['hourly']['time']
-precipitation = data['hourly']['rain']
+  if response2.status_code == 200:
+    
+    data = response2.json()
+    time = data['hourly']['time']
+    precipitation = data['hourly']['rain']
+    
+    # Plot rain data
+    plt.figure(figsize=(50, 10))
+    plt.plot(time, precipitation, marker='o', linestyle='-')
+    plt.title('Rain in ' + location['city'])
+    plt.xlabel('Time ')
+    plt.ylabel('Rain precipitation (mm)')
+    plt.grid(True)
+    plt.xticks(rotation=45)
+    plt.show()
+    
+  else:
+    print("Error getting rain data")
 
-# Plot rain data
-plt.figure(figsize=(50, 10))
-plt.plot(time, precipitation, marker='o', linestyle='-')
-plt.title('Rain in ' + location['city'])
-plt.xlabel('Time ')
-plt.ylabel('Rain precipitation (mm)')
-plt.grid(True)
-plt.xticks(rotation=45)
-plt.show()
+else:
+  print(f"Oh nooose something went wrong: {response.status_code} - {response.reason}")
